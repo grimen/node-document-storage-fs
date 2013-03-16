6c382af52251268aa7b882b2b2bfde241921e337
Jonas Grimfelt <grimen@gmail.com>
2013-03-16 17:23:20 +0100

    Make connection URL protocol map to storage adapter.

--- test/index.js
+++ test/index.js
@@ -8,6 +8,8 @@ var Storage = require('node-document-storage');
 module.exports = Storage.Spec('FS', {
   module: require('..'),
   engine: require('node-fs'),
+  id: 'fs',
+  protocol: 'file',
   db: '/tmp/default-test',
   default_url: 'file:///tmp/default-test',
   authorized_url: undefined,
